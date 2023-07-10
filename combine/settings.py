44fb8cfd6bc829cea68fdef31b94682988804066
Trilok Thakar <118374042+trilok-thakar@users.noreply.github.com>
2023-07-10 14:31:44 +0530

    Update settings.py
    Update SMTP

--- combine/settings.py
+++ combine/settings.py
@@ -128,5 +128,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'jordenjasifa@gmail.com'
-EMAIL_HOST_PASSWORD = 'tjzvawnkieqlrfxr'
+EMAIL_HOST_USER = # Enter Your Email Host
+EMAIL_HOST_PASSWORD = # Enter Your Password
